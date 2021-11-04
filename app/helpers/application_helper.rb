aa497be351daab67c4fe8854c913c856c78f27c5
Tine <zalozniktine59@gmail.com>
2021-11-04 17:14:42 +0100

    refaktoiranje helperjev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def display_session_links
+        capture do
+            if user_signed_in?
+                concat link_to "Odjava", destroy_user_session_path, method: :delete
+            else
+                concat link_to "Prijava", new_user_session_path
+                concat "  " 
+                concat link_to "Registracija", new_user_registration_path 
+                concat "  " 
+            end 
+        end
+    end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,21 @@
 module PostsHelper
+    def display_index_post_links(post)
+        capture do
+            concat content_tag(:td, link_to('Show', post))
+            if current_user == post.user
+                concat content_tag(:td, link_to('Edit', edit_post_path(post)))
+                concat content_tag(:td, link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+            end
+        end
+    end 
+
+    def display_show_post_links(post)
+        capture do
+            if current_user == post.user
+                concat link_to 'Edit', edit_post_path(post) 
+                concat '  |  '
+            end
+            concat link_to 'Back', posts_path
+    end
+end
 end
