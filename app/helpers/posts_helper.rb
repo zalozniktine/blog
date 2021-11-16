50659bf33e41ef9b2df89f3d466ed85303d9f51b
Tine <zalozniktine59@gmail.com>
2021-11-16 13:52:28 +0100

    popravek

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,8 +3,7 @@ module PostsHelper
         capture do
             concat link_to('Preberi vec', post, class: 'stretched-link')
             if current_user == post.user
-                concat link_to('Uredi', edit_post_path(post), class: 'stretched-link')
-                concat link_to('Izbrisi', post, method: :delete, data: { confirm: 'Are you sure?' })
+                
             end
         end
     end 
@@ -12,7 +11,9 @@ module PostsHelper
     def display_show_post_links(post)
         capture do
             if current_user == post.user
-                concat link_to 'Edit', edit_post_path(post) 
+                concat link_to 'Izbrisi', post, method: :delete, data: { confirm: 'Are you sure?' }
+                concat '  |  '
+                concat link_to 'Uredi', edit_post_path(post)
                 concat '  |  '
             end
             concat link_to 'Back', posts_path
