2c903b1519a8fba3dd19a7f17f4ffed5a22b30c2
Tine <zalozniktine59@gmail.com>
2021-11-16 12:30:21 +0100

    add user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
     def display_session_links
         capture do
             if user_signed_in?
+                concat link_to "Uredi profil", edit_user_registration_path
+                concat "  " 
                 concat link_to "Odjava", destroy_user_session_path, method: :delete
             else
                 concat link_to "Prijava", new_user_session_path
