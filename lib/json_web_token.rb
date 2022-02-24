6e7c4cb77325d2d2e6679072ebebee16fcd500ad
Tine <zalozniktine59@gmail.com>
2022-02-24 09:42:35 +0100

    Add env

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,12 +2,12 @@ class JsonWebToken
     class << self
       def encode(payload, expiry = 24.hours.from_now)
         payload[:expiry] = expiry.to_i
-        secret_key_base = Rails.application.secrets.secret_key_base
+        secret_key_base = ENV['SECURE_KEY']
         JWT.encode(payload, secret_key_base)
       end
       
       def decode(token)
-        secret_key_base = Rails.application.secrets.secret_key_base
+        secret_key_base = ENV['SECURE_KEY']
         body = JWT.decode(token, secret_key_base)[0]
         HashWithIndifferentAccess.new body
       rescue
